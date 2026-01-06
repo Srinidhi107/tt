ffffbf990f40b80d71e99e7c60f127555dbd13b0
vikramdev772 <vikramdev772@gmail.com>
2026-01-06 15:31:43 +0530

    Signup

--- client/src/auth/Signup.jsx
+++ client/src/auth/Signup.jsx
@@ -1,55 +1,66 @@
 import React, { useState } from "react";
 
 function Signup() {
-  let [n, setN] = useState("username");
-  let [e, setE] = useState("mail");
-  let [p, setP] = useState("pass");
+  const [n, setN] = useState("");
+  const [e, setE] = useState("");
+  const [p, setP] = useState("");
 
-    function submitHandeler(){
-        let obj={
-            "name":n,
-            "email":e,
-            "password":p
-        }
-        console.log(obj);
+  async function submitHandeler() {
+    const obj = {
+      name: n,
+      email: e,
+      password: p,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      });
+
+      const data = await response.text();
+      console.log("Response:", data);
+      alert("Signup successful");
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Signup failed");
     }
+  }
 
   return (
-    <>
-      <div>
-        <h3>create account</h3>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(e) => {
-            setN(e.target.value);
-          }}
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="email"
-          onChange={(e) => {
-            setE(e.target.value);
-          }}
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="password"
-          onChange={(e) => {
-            setP(e.target.value);
-          }}
-        />
-        <br />
-        <br />
-        <button onClick={submitHandeler}>submit</button>
-        <br />
-        <h4>name : {n}</h4>
-        <h4>email : {e}</h4>
-        <h4>password : {p}</h4>
-      </div>
-    </>
+    <div>
+      <h3>Create Account</h3>
+
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setN(e.target.value)}
+      />
+      <br />
+
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setE(e.target.value)}
+      />
+      <br />
+
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setP(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={submitHandeler}>Submit</button>
+
+      <h4>name : {n}</h4>
+      <h4>email : {e}</h4>
+      <h4>password : {p}</h4>
+    </div>
   );
 }
 
