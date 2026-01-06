1e60b79f3fd4fba252543d5f87117b33836a2a10
vikramdev772 <vikramdev772@gmail.com>
2026-01-06 14:47:06 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route  path="/"  element={} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
